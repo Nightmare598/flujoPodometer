4c2dd87e56d009e71671b266b5c112bc3c863532
Sebastian Morales Yabeta <a2018113411@estudiantes.upsa.edu.bo>
2021-03-15 02:09:51 -0400

    Agregar archivos de proyecto.

---
+++ Controllers/DataController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+using apiProductor.Models;
+using Newtonsoft.Json;
+
+namespace apiProductor.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        public async Task<string> PostAsync([FromBody] Data data)
+        {
+            string connectionString = "Endpoint=sb://queuepodometer.servicebus.windows.net/;SharedAccessKeyName=Escuchar;SharedAccessKey=vItB/2VHDRn/bnv+xSYISzJpbTQm7/W7oJck76Jjn58=;EntityPath=cola1";
+            string queueName = "cola1";
+            string mensaje = JsonConvert.SerializeObject(data);
+
+            // create a Service Bus client 
+            await using (ServiceBusClient client = new ServiceBusClient(connectionString))
+            {
+                // create a sender for the queue 
+                ServiceBusSender sender = client.CreateSender(queueName);
+
+                // create a message that we can send
+                ServiceBusMessage message = new ServiceBusMessage(mensaje);
+
+                // send the message
+                await sender.SendMessageAsync(message);
+                Console.WriteLine($"Sent a single message to the queue: {queueName}");
+            }
+            return "Mensaje enviado con exito";
+        }
+    }
+}
---
+++ Models/Data.cs
@@ -0,0 +1,17 @@
+﻿namespace apiProductor.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    public class Data
+    {
+        [Key]
+        public string Email { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime EventDate { get; set; }
+        [Required]
+        public int Steps { get; set; }
+    }
+}
